ed824fd8201788b27266bd967f8a885af37f3a63
Kaila Prochaska <kailaprochaska@90.dhcp.apogeetelecom.com>
2018-09-08 22:39:17 -0500

    commiting changes made to tumblr

---
+++ tumblr/PhotosCell.swift
@@ -0,0 +1,25 @@
+//
+//  PhotosCell.swift
+//  tumblr
+//
+//  Created by Kaila Prochaska on 9/7/18.
+//  Copyright © 2018 Kaila Prochaska. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCell: UITableViewCell {
+
+    @IBOutlet weak var myImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
